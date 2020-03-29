6ac9b480d34f24f5a144411e1119167a0856f3e5
Saransh <saranshseth93@gmail.com>
2020-03-29 22:38:35 +0530

    Updates

---
+++ precache-manifest.9a99f9324a63f70d3b51e173122eacea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb511cd0c0aa6d06ba36d686f7825f0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a67cd0781cd8682c7182",
+    "url": "/monsters-rolodex/static/css/main.86c13607.chunk.css"
+  },
+  {
+    "revision": "b7469e42d9182eb24593",
+    "url": "/monsters-rolodex/static/js/2.b3d500ed.chunk.js"
+  },
+  {
+    "revision": "a67cd0781cd8682c7182",
+    "url": "/monsters-rolodex/static/js/main.1f489fad.chunk.js"
+  },
+  {
+    "revision": "7548f6fbafc46e9f04b0",
+    "url": "/monsters-rolodex/static/js/runtime~main.4eb04847.js"
+  }
+]);
